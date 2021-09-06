482ccb8e00ad38d0efd54ffdb7de0435fc0851be
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-06 11:13:25 -0300

    add basic structure for todo list

---
+++ src/TodoApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TodoApp = () => {
+    return (
+        <div className="page">
+            <div className="item-list">Hola</div>
+            <hr />
+            <div className="item-list">Hola</div>
+            <hr />
+            <div className="item-list">Hola</div>
+            <hr />
+            <div className="item-list">Hola</div>
+            <hr />
+
+        </div>
+    )
+}
