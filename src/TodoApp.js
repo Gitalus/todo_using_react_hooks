e52dd3d4253a5b72771e2f922ec51b640808367f
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-06 11:39:47 -0300

    complete basic todoList using custom Hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from './hooks/useForm';
 
 export const TodoApp = () => {
+
+    const [todos, setTodos] = useState([]);
+
+
+    const [ { desc }, handleInputChange, reset ] = useForm({
+        desc: ''
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newTodo = {
+            id: new Date().getTime(),
+            desc
+        }
+        setTodos([ ...todos, newTodo ]);
+        reset();
+    };
+
+
+
     return (
         <div className="page">
-            <div className="item-list">Hola</div>
-            <hr />
-            <div className="item-list">Hola</div>
-            <hr />
-            <div className="item-list">Hola</div>
-            <hr />
-            <div className="item-list">Hola</div>
-            <hr />
+            <form onSubmit={ handleSubmit }>
+                <input 
+                    className="item-list"
+                    type="text"
+                    value={ desc }
+                    onChange={ handleInputChange }
+                    name="desc"
+                />
+                <hr />
+            </form>
+            {
+                todos.map( (todo) => (
+                    <>
+                        <div 
+                            key={ todo.id }
+                            className="item-list">
+                                { todo.desc }
+                        </div>
+                        <hr />
+                    </>
+                ))
+            }
 
         </div>
     )
